e4cc7e46be2a4f7b5aa9eb98a8e638cfd5112f6a
Diogo Teixeira <diogo_alex9@hotmail.com>
2020-03-07 15:54:49 +0000

    Plane movement angles working

--- Assets/Scripts/PlaneController.cs
+++ Assets/Scripts/PlaneController.cs
@@ -9,40 +9,45 @@ public class PlaneController : MonoBehaviour
     public float verticalAngleMod;
     public float horizontalAngleMod;
     private float angle;
-
-    private Vector3 origVectorPosition;
+    private Vector3 vectorX;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody2D>();
-        origVectorPosition = transform.position;
+        vectorX = new Vector3(1, 0, 0);
+        rigidBody.AddForce(transform.right * 1, ForceMode2D.Impulse);
+        angle = getAngle();
+        rigidBody.SetRotation(-angle);
     }
 
     // Update is called once per frame
     void Update()
     {
-        angle = rigidBody.transform.rotation.z;
-        rigidBody.velocity = new Vector2(rigidBody.velocity.x + Mathf.Cos(angle) * horizontalAngleMod, rigidBody.velocity.y + Mathf.Sin(angle) * verticalAngleMod);
+        angle = getAngle();
+        Debug.Log(angle);
+        Debug.Log(Mathf.Sin(angle * Mathf.Deg2Rad));
+        rigidBody.SetRotation(angle);
+        //rigidBody.velocity += new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad) * horizontalAngleMod,- Mathf.Sin(angle * Mathf.Deg2Rad) * verticalAngleMod);
 
-        Vector3 moveDirection = transform.position - origVectorPosition;
-
-        if (moveDirection != Vector3.zero)
-        {
-            Vector3 lookAtPoint = gameObject.transform.position + moveDirection;
-            transform.LookAt(lookAtPoint, Vector2.up);  //assuming y-axis is up in your game
-
-        }
 
     }
 
     void OnTriggerStay2D(Collider2D other)
     {
-        if (other.tag == "Fan")
+        if (other.CompareTag("Fan"))
         {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y + fanSpeed);
+            rigidBody.AddForce(new Vector2(0, 1) * fanSpeed, ForceMode2D.Force);
         }
     }
 
-
+    private float getAngle()
+    {
+        Vector3 moveVector = new Vector3(rigidBody.velocity.x, rigidBody.velocity.y, 0);
+        if (Mathf.Abs(moveVector.x) < 0.1 && Mathf.Abs(moveVector.y) < 0.1)
+        {
+            return -90;
+        }
+        return Vector3.SignedAngle(vectorX, moveVector, Vector3.forward);
+    }
 }
