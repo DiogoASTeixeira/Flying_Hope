f6b44ec5bb9818a5f2cdbda873179d3d9b4dcfa9
Fábio Azevedo <fabioalexandre7b12@gmail.com>
2020-03-22 16:12:36 +0000

    OptionsMenu added with volume slider

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -15,6 +15,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void Options()
+    {
+        
+    }
+
     public void QuitGame()
     {
         Application.Quit();
