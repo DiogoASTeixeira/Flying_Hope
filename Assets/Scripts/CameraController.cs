bce4f641aaa2da46cb9c0ff0186ca922eebd194a
Diogo Teixeira <diogo_alex9@hotmail.com>
2020-03-23 15:26:32 +0000

    New outdoor ceiling and camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,29 +10,16 @@ public class CameraController : MonoBehaviour
     public float offsetSmoothing;
     private Rigidbody2D playerRB;
 
-    public GameObject[] levels;
-
-    private Camera mainCamera;
-    private Vector2 screenBounds;
-    public float choke;
-
     // Start is called before the first frame update
     void Start()
     {
         playerRB = player.GetComponent<Rigidbody2D>();
-
-        mainCamera = gameObject.GetComponent<Camera>();
-        screenBounds = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, mainCamera.transform.position.z));
-        foreach (GameObject obj in levels)
-        {
-            LoadChildObjects(obj);
-        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        playerPosition = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
         if (playerRB.velocity.x >= -1f)
         {
             playerPosition = new Vector3(player.transform.position.x + offset, playerPosition.y, playerPosition.z);
@@ -46,46 +33,6 @@ public class CameraController : MonoBehaviour
 
     private void LateUpdate()
     {
-        foreach (GameObject obj in levels)
-        {
-            RepositionChildObjects(obj);
-        }
-    }
 
-    private void LoadChildObjects(GameObject obj)
-    {
-        float objectWidth = obj.GetComponent<SpriteRenderer>().bounds.size.x - choke;
-        int childsNeeded = (int)Mathf.Ceil(screenBounds.x * 2 / objectWidth);
-        GameObject clone = Instantiate(obj) as GameObject;
-        for (int i = 0; i <= childsNeeded; i++)
-        {
-            GameObject c = Instantiate(clone) as GameObject;
-            c.transform.SetParent(obj.transform);
-            c.transform.position = new Vector3(objectWidth * i, obj.transform.position.y, obj.transform.position.z);
-            c.name = obj.name + i;
-        }
-        Destroy(clone);
-        Destroy(obj.GetComponent<SpriteRenderer>());
-    }
-
-    private void RepositionChildObjects(GameObject obj)
-    {
-        Transform[] children = obj.GetComponentsInChildren<Transform>();
-        if (children.Length > 1)
-        {
-            GameObject firstChild = children[1].gameObject;
-            GameObject lastChild = children[children.Length - 1].gameObject;
-            float halfObjectWidth = lastChild.GetComponent<SpriteRenderer>().bounds.extents.x - choke;
-            if (transform.position.x + screenBounds.x > lastChild.transform.position.x + halfObjectWidth)
-            {
-                firstChild.transform.SetAsLastSibling();
-                firstChild.transform.position = new Vector3(lastChild.transform.position.x + halfObjectWidth * 2, lastChild.transform.position.y, lastChild.transform.position.z);
-            }
-            else if (transform.position.x - screenBounds.x < firstChild.transform.position.x - halfObjectWidth)
-            {
-                lastChild.transform.SetAsFirstSibling();
-                lastChild.transform.position = new Vector3(firstChild.transform.position.x - halfObjectWidth * 2, firstChild.transform.position.y, firstChild.transform.position.z);
-            }
-        }
     }
 }
